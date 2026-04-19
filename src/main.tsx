871468ea248d245787cc7b49c42d5e66f49449bf
kevin1kevin1k <kevin1kevin1k@gmail.com>
2026-04-19 12:38:05 +0800

    feat(auth): add Supabase client and AuthContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { initDB } from './db/init'
+import { AuthProvider } from './contexts/AuthContext'
 
 initDB().catch(console.error)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
